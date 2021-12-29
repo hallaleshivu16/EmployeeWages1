23d2eb3b45fdabe153ce0ab7caa8c665d80650d9
shivkumar <hallaleshivu16@gmail.com>
2021-12-30 01:26:36 +0530

    Added: Employee Daily WAge

--- src/com/bridgelabz/emplyeeWages1/EmployeeWages1.java
+++ src/com/bridgelabz/emplyeeWages1/EmployeeWages1.java
@@ -1,13 +1,21 @@
 package com.bridgelabz.emplyeeWages1;
 
 public class EmployeeWages1 {
+    //Constants are called inside the class only
+    private static int IS_PRESENT = 1;
+    private static int WAGE_PER_HOUR = 20;
+    private static int FULL_DAY_HOUR = 8;
+
     public static void main(String[] args) {
-        int IS_PRESENT = 1;
+
         int empCheck = (int) Math.floor(Math.random() * 10) %2;
         if( empCheck == IS_PRESENT ) {
             System.out.println("Employee is Present");
+            int empWage = (int) Math.floor(WAGE_PER_HOUR * FULL_DAY_HOUR);
+            System.out.println("Employee Daily Wage:" +empWage);
         } else {
             System.out.println("Employee is Absent");
+            System.out.println("Employee DAily Wage:" +0 );
         }
     }
 }
