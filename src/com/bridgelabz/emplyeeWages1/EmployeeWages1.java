9f47e7a889bf98dfc9be4123c89216cffe93e8f9
shivkumar <hallaleshivu16@gmail.com>
2021-12-30 19:16:05 +0530

    ADDED: Employee Part Time Daily Wages

--- src/com/bridgelabz/emplyeeWages1/EmployeeWages1.java
+++ src/com/bridgelabz/emplyeeWages1/EmployeeWages1.java
@@ -1,13 +1,24 @@
 package com.bridgelabz.emplyeeWages1;
 
 public class EmployeeWages1 {
+    private static int IS_FULL_TIME = 1;
+    //private static int IS_PART_TIME = 2;
+    //private static int FULL_DAY_HOURS = 8;
+    private static int WAGE_PER_HOURS = 20;
+    //private static int PART_DAY_HOURS = 4;
+    private static int workingHours = 0;
+    //private static int employeeDailyWages = 0;
+
     public static void main(String[] args) {
-        int IS_PRESENT = 1;
-        int empCheck = (int) Math.floor(Math.random() * 10) %2;
-        if( empCheck == IS_PRESENT ) {
-            System.out.println("Employee is Present");
+        int empCheck = (int) Math.floor(Math.random() * 10) %3;
+        if( empCheck == IS_FULL_TIME ) {
+            workingHours = 8;
+            System.out.println("Employee is worked as Full Time");
         } else {
-            System.out.println("Employee is Absent");
+            workingHours = 4;
+            System.out.println("Employee is worked as Part Time");
         }
+            int employeeDailyWages = (int) Math.floor(workingHours * WAGE_PER_HOURS);
+            System.out.println("Employee Daily Wages:" +employeeDailyWages);
     }
 }
