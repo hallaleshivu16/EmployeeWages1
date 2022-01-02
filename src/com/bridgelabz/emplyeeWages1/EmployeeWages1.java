ff15ab7221c49394efee49ade4a9dab1cdb6c642
shivkumar <hallaleshivu16@gmail.com>
2022-01-02 11:51:24 +0530

    ADDED; Wages till Hours or Day Reaches

--- src/com/bridgelabz/emplyeeWages1/EmployeeWages1.java
+++ src/com/bridgelabz/emplyeeWages1/EmployeeWages1.java
@@ -6,16 +6,20 @@ public class EmployeeWages1 {
     private static final int IS_PART_TIME = 2;
     private static final int WAGE_PER_HOUR = 20;
     private static final int WORKING_DAY_PER_MONTH = 20;
+    private static final int WOKING_HOURS = 100;
 
     public static void main(String[] args) {
         //Initiating Variables
         int empWages = 0;
         int empHours = 0;
         int totalWages = 0;
+        int totalHours = 0;
+        int i = 1;
 
-        //Running for Loop to Compute Monthly Salary
-        for (int i = 1; i <= WORKING_DAY_PER_MONTH; i ++) {
-
+        //Running while Loop to Reach Working Day or Maximum Working Hour
+        while ( i <= WORKING_DAY_PER_MONTH && totalHours < WOKING_HOURS) {
+            System.out.println("total Hours:" +totalHours);
+            System.out.println("Woking Days:" +i);
             //Employee Presentee calculation
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -31,9 +35,10 @@ public class EmployeeWages1 {
                     System.out.println("Employee has not Worked");
                     empHours = 0;
             }
-            empWages = empHours * WAGE_PER_HOUR;
-            totalWages = empWages + totalWages;
+            i++;
+            totalHours = totalHours + empHours;
         }
-            System.out.println("Employee Monthly Wages:" +totalWages);
+        totalWages = totalHours * WAGE_PER_HOUR;
+        System.out.println("Employee Monthly Wages:" +totalWages);
     }
 }
